beacbdd9e43e4041a43a2b66439fe7840c9a64b9
Elder Moraes <elder.moraes@gmail.com>
2019-08-29 18:20:35 -0300

    Fixes

--- ch-02/item01staticfactorymethods/src/main/java/com/eldermoraes/item01staticfactorymethods/Naming.java
+++ ch-02/item01staticfactorymethods/src/main/java/com/eldermoraes/item01staticfactorymethods/Naming.java
@@ -23,36 +23,74 @@ package com.eldermoraes.item01staticfactorymethods;
  * @author eldmorae
  */
 public class Naming {
-    
-    private Naming(){
-        
+
+    private final String s1;
+    private final String s2;
+    private final String s3;
+    private final String s4;
+    private final Integer i1;
+    private final Integer i2;
+
+    private Naming() {
+        this.s1 = null;
+        this.s2 = null;
+        this.s3 = null;
+        this.s4 = null;
+        this.i1 = null;
+        this.i2 = null;
     }
-    
-    private Naming(String s1, String s2, Integer i1){
-        
+
+    private Naming(String s1, String s2, Integer i1) {
+        this.s1 = s1;
+        this.s2 = s2;
+        this.s3 = null;
+        this.s4 = null;
+        this.i1 = i1;
+        this.i2 = null;
     }
-    
-    private Naming(String s1, Integer i1, String s2){
-        
+
+    private Naming(String s3, Integer i2, String s4) {
+        this.s1 = null;
+        this.s2 = null;
+        this.s3 = s3;
+        this.i1 = null;
+        this.s4 = s4;
+        this.i2 = i2;
     }
-    
-    public static Naming noParam(){
+
+    public static Naming noParam() {
         return new Naming();
     }
-    
-    public static Naming twoStringsOneInteger(String s1, String s2, Integer i1){
+
+    public static Naming sOneStwoIone(String s1, String s2, Integer i1) {
         return new Naming(s1, s2, i1);
     }
-    
-    public static Naming stringIntegerString(String s1, Integer i1, String s2){
-        return new Naming(s1, i1, s2);
-    }
-}
 
-class Test{
-    public static void main(String[] args) {
-        Naming n1 = Naming.noParam();
-        Naming n2 = Naming.stringIntegerString("", 1, "");
-        Naming n3 = Naming.twoStringsOneInteger("", "", 1);
+    public static Naming sThreeItwoSfour(String s3, Integer i2, String s4) {
+        return new Naming(s3, i2, s4);
+    }
+
+    public String getS1() {
+        return s1;
+    }
+
+    public String getS2() {
+        return s2;
+    }
+
+    public String getS3() {
+        return s3;
+    }
+
+    public String getS4() {
+        return s4;
+    }
+
+    public Integer getI1() {
+        return i1;
+    }
+
+    public Integer getI2() {
+        return i2;
     }
 }
